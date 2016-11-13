f22475ab6f549eafe1ddd549aa8a3c34a661f447
wtanaka.com <wtanaka@yahoo.com>
2016-11-12 21:49:05 -1000

    Initial build steps

--- Makefile
+++ Makefile
@@ -1,4 +1,17 @@
-all: cabal.sandbox.config
+all: package
+
+clean:
+	find . -name "*~" -exec rm \{\} \;
+	rm -rf dist
+
+realclean: clean
+	cabal sandbox delete
+
+package: dist/build/proto2graphql/proto2graphql
+
+dist/build/proto2graphql/proto2graphql: cabal.sandbox.config
+	cabal install --only-dependencies
+	cabal build
 
 .cabal-sandbox cabal.sandbox.config:
 	cabal sandbox init
---
+++ src/Main.hs
@@ -0,0 +1,27 @@
+#!/usr/bin/env runhaskell
+--
+-- Copyright 2016 Wesley Tanaka <http://wtanaka.com/>
+--
+-- This file is part of gitstat.
+--
+-- gitstat is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- gitstat is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with gitstat.  If not, see <http://www.gnu.org/licenses/>.
+import qualified Data.ByteString.Lazy as BSL
+
+proto2graphql :: BSL.ByteString -> BSL.ByteString
+proto2graphql x = x
+
+main :: IO ()
+main = do
+   BSL.interact $ proto2graphql
+
